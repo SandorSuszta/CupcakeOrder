dcf5a240a9422d51cbf4ffea7fe1d2f3942bc0a1
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-17 22:38:50 +0100

    Initial Commit

---
+++ CupcakesOrder/CupcakesOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakesOrderApp.swift
+//  CupcakesOrder
+//
+//  Created by Nataliia Shusta on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakesOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
