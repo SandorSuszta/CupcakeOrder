10740ee3ca4702bf2c33ee71a12a36a7cddfe109
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-17 22:45:02 +0100

    Create OrderView

---
+++ CupcakesOrder/OrderView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct OrderView: View {
+    @ObservedObject var order: Order
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink {
+                    //
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            OrderView(order: Order())
+        }
+    }
+}
